123dddda88b5c92811de76b284a7e89a07cdd07c
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-14 10:54:59 +0530

    #updateByDz

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import { ReactComponent as HomeIcon } from '../assets/svg/HomeSVG.svg';
 import { ReactComponent as LocationIcon } from '../assets/svg/LocationSVG.svg';
 import { ReactComponent as CategoriesIcon } from '../assets/svg/CategoriesSVG.svg';
 import { ReactComponent as UserIcon } from '../assets/svg/UserSVG.svg';
+import '../styles/navbar.css';
 
 const Navbar = () => {
   return (
-    <nav className='bg-myGrey rounded-[0.625rem]'>
+    <nav className='bg-myGrey rounded-[0.625rem] w-fit group'>
       <ul className='flex flex-col gap-5 p-5'>
-        <li className='flex items-center justify-start gap-3'><HomeIcon height={24} width={24}/><span>Home</span></li>
-        <li className='flex items-center justify-start gap-3'><LocationIcon height={24} width={24}/><span>Location</span></li>
-        <li className='flex items-center justify-start gap-3'><CategoriesIcon height={24} width={24}/><span>Categories</span></li>
-        <li className='flex items-center justify-start gap-3'><UserIcon height={24} width={24}/><span>User</span></li>
+        <li className='flex items-center justify-start gap-3'><HomeIcon height={24} width={24}/><span className='hidden group-hover:block'>Home</span></li>
+        <li className='flex items-center justify-start gap-3'><LocationIcon height={24} width={24}/><span className='hidden group-hover:block'>Location</span></li>
+        <li className='flex items-center justify-start gap-3'><CategoriesIcon height={24} width={24}/><span className='hidden group-hover:block'>Categories</span></li>
+        <li className='flex items-center justify-start gap-3'><UserIcon height={24} width={24}/><span className='hidden group-hover:block'>User</span></li>
       </ul>
     </nav>
   )
