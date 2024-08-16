996985c9473a8b93eead117da5f4d43475fafcc2
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-16 17:09:45 +0530

    #updateByDz

---
+++ client/src/pages/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Post = () => {
+    return (
+        <div className='flex items-center justify-center w-screen h-screen'>
+            <div className='container h-full flex flex-col'>
+                <span>Post your Ad</span>
+                <span>Cars, Mobiles and Bikes</span>
+            </div>
+        </div>
+    )
+}
+
+export default Post
