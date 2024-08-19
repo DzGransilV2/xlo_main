65a05dc0883a2355357ec57cd66f7c3b551885a8
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-19 20:50:02 +0530

    #updateByDz

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Searchbar from './Searchbar'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -13,12 +14,12 @@ const Header = () => {
                         {/* <input className='w-full h-full rounded-[0.625rem] p-5' type="text" name="" id="" placeholder='Type here to search...'/> */}
                         <Searchbar />
                     </div>
-                    <button className='bg-myGrey px-4 h-[3.125rem] flex items-center justify-center gap-[0.3125rem] rounded-[0.625rem] hover:shadow-myShadow hover:cursor-pointer'>
+                    <Link to='/post' className='bg-myGrey px-4 h-[3.125rem] flex items-center justify-center gap-[0.3125rem] rounded-[0.625rem] hover:shadow-myShadow hover:cursor-pointer'>
                         <span>
                             <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24"><path fill="currentColor" d="m21.4 14.25l-7.15 7.15q-.3.3-.675.45t-.75.15t-.75-.15t-.675-.45l-8.825-8.825q-.275-.275-.425-.637T2 11.175V4q0-.825.588-1.412T4 2h7.175q.4 0 .775.163t.65.437l8.8 8.825q.3.3.438.675t.137.75t-.137.738t-.438.662M12.825 20l7.15-7.15L11.15 4H4v7.15zM6.5 8q.625 0 1.063-.437T8 6.5t-.437-1.062T6.5 5t-1.062.438T5 6.5t.438 1.063T6.5 8m5.5 4"></path></svg>
                         </span>
                         <span className='font-medium text-[1.5rem]'>Sell</span>
-                    </button>
+                    </Link>
                 </div>
             </header>
         </div>
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Form from '../components/Form'
 import { ReactComponent as ArrowBackIcon } from '../assets/svg/ArrowBackSVG.svg';
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom';
 
 const Post = () => {
     return (
@@ -9,12 +10,12 @@ const Post = () => {
             <section className='container relative w-full h-full flex flex-col items-center justify-center gap-5'>
                 {/* <span>Cars, Mobiles and Bikes</span> */}
                     <div className='w-full flex absolute top-[10%]'>
-                        <button className='bg-myGrey px-4 h-[3.125rem] flex items-center justify-center gap-[0.3125rem] rounded-[0.625rem] hover:shadow-myShadow hover:cursor-pointer'>
+                        <Link to='/' className='bg-myGrey px-4 h-[3.125rem] flex items-center justify-center gap-[0.3125rem] rounded-[0.625rem] hover:shadow-myShadow hover:cursor-pointer'>
                             <span>
                                 <ArrowBackIcon width={24} height={24}/>
                             </span>
                             <span className='font-medium text-base'>Back</span>
-                        </button>
+                        </Link>
                     </div>
                     <span className='font-bold text-2xl'>Post</span>
                     <Form />
