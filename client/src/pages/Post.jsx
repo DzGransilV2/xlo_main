821f7c7946304c9bcd238b60d1bf7707fee40414
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-19 09:26:08 +0530

    #updateByDz

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Form from '../components/Form'
 import { ReactComponent as ArrowBackIcon } from '../assets/svg/ArrowBackSVG.svg';
+import Footer from '../components/Footer'
 
 const Post = () => {
     return (
         <div className='flex items-center justify-center w-screen h-screen'>
-            <section className='container h-full flex flex-col items-center justify-center gap-5'>
+            <section className='container relative w-full h-full flex flex-col items-center justify-center gap-5'>
                 {/* <span>Cars, Mobiles and Bikes</span> */}
                     <div className='w-full flex'>
                         <button className='bg-myGrey px-4 h-[3.125rem] flex items-center justify-center gap-[0.3125rem] rounded-[0.625rem] hover:shadow-myShadow hover:cursor-pointer'>
@@ -17,6 +18,9 @@ const Post = () => {
                     </div>
                     <span className='font-bold text-2xl'>Post</span>
                     <Form />
+                    <div className='absolute w-full bottom-0'>
+                        <Footer />
+                    </div>
             </section>
         </div>
     )
