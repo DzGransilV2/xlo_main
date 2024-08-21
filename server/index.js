812ee7f6468c74500841bf616a3c34591d19b5fd
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-21 20:45:00 +0530

    #updateByDz

--- server/index.js
+++ server/index.js
@@ -1,41 +1,17 @@
 const express = require("express");
+const cors = require('cors')
+require('./config/config');
+const User = require('./models/User');
 const app = express();
-const mongoose = require("mongoose");
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/xlo').then(
-            console.log("Connected to MongoDB")
-        );
-        const userSchema = new mongoose.Schema({
-            uname: String
-        });
-        const users = mongoose.model('users', userSchema);
-        const data = await users.find().then(
-            console.log("DATA FOUND")
-        );
-        console.log(data)
-    } catch (error) {
-        console.error("Error occurred:", error);
-    }
-};
 
-connectDB();
+app.use(express.json());
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>App in Progress</title>
-        </head>
-        <body>
-            <h1>App in progress</h1>
-        </body>
-        </html>
-    `);
+app.post('/signup', async (req, res)=>{
+    let user = new User(req.body);
+    let result = await user.save();
+    res.send(result);
 });
 
 app.listen(8000, function() {
