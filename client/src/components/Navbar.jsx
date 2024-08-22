c10a2ceb17dc6325180b02864c957f4e103eda95
DzGransilV2 <gransildsouza2@gmail.com>
2024-08-22 15:18:50 +0530

    #updateByDz

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,26 +9,26 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className='navbar bg-myGrey rounded-[0.625rem] group'>
-      <ul className='flex flex-col gap-5 p-5'>
-        <li className='nav-item'>
+      <ul className='flex flex-col gap-5 p-4'>
+        <li className='nav-item text-hover'>
           <Link to='/home' className='nav-item'>
             <span><HomeIcon height={24} width={24} /></span>
             <span className='nav-text'>Home</span>
           </Link>
         </li>
-        <li className='nav-item'>
+        <li className='nav-item text-hover'>
           <Link className='nav-item'>
             <span><LocationIcon height={24} width={24} /></span>
             <span className='nav-text'>Location</span>
           </Link>
         </li>
-        <li className='nav-item'>
+        <li className='nav-item text-hover'>
           <Link className='nav-item'>
             <span><CategoriesIcon height={24} width={24} /></span>
             <span className='nav-text'>Categories</span>
           </Link>
         </li>
-        <li className='nav-item'>
+        <li className='nav-item text-hover'>
           <Link to='/profile' className='nav-item'>
             <span><UserIcon height={24} width={24} /></span>
             <span className='nav-text'>Profile</span>
