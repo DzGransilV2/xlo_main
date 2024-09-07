ae7bbb7a465ff5429091d4fcc32128e337e1ab51
DzGransilV2 <gransildsouza2@gmail.com>
2024-09-07 17:25:50 +0530

    #updateNav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,9 +11,9 @@ const Navbar = () => {
   const [activeLink, setActiveLink] = useState('');
   const location = useLocation();
 
-  const handleSetActiveLink = (link) => {
-    setActiveLink(link);
-  };
+  // const handleSetActiveLink = (link) => {
+  //   setActiveLink(link);
+  // };
 
   useEffect(() => {
     const path = location.pathname;
@@ -27,26 +27,30 @@ const Navbar = () => {
   return (
     <nav className='navbar bg-myGrey md:border-none border-2 border-white rounded-[0.625rem] md:shadow-none shadow-myShadow group'>
       <ul className='flex flex-row md:flex-col gap-5 p-[0.5rem]'>
+        {/* onClick={()=>handleSetActiveLink('home')} */}
         <li className={`nav-item text-hover ${activeLink==='home'?'active': ''}`}>
-          <Link onClick={()=>handleSetActiveLink('home')} to='/home' className='nav-item'>
+          <Link to='/home' className='nav-item'>
             <span><HomeIcon height={24} width={24} /></span>
             <span className='nav-text'>Home</span>
           </Link>
         </li>
+        {/* onClick={()=>handleSetActiveLink('location')} */}
         <li className={`nav-item text-hover ${activeLink==='location'?'active': ''}`}>
-          <Link onClick={()=>handleSetActiveLink('location')} className='nav-item'>
+          <Link className='nav-item'>
             <span><LocationIcon height={24} width={24} /></span>
             <span className='nav-text'>Location</span>
           </Link>
         </li>
+        {/* onClick={()=>handleSetActiveLink('categories')}  */}
         <li className={`nav-item text-hover ${activeLink==='categories'?'active': ''}`}>
-          <Link onClick={()=>handleSetActiveLink('categories')} className='nav-item'>
+          <Link className='nav-item'>
             <span><CategoriesIcon height={24} width={24} /></span>
             <span className='nav-text'>Categories</span>
           </Link>
         </li>
+        {/* onClick={()=>handleSetActiveLink('profile')} */}
         <li className={`nav-item text-hover ${activeLink==='profile'?'active': ''}`}>
-          <Link onClick={()=>handleSetActiveLink('profile')} to={`/profile`} className='nav-item'>
+          <Link to={`/profile`} className='nav-item'>
             <span><UserIcon height={24} width={24} /></span>
             <span className='nav-text'>Profile</span>
           </Link>
