515b7a441d8d8cd6c350097915608ae9aa0085f2
DzGransilV2 <gransildsouza2@gmail.com>
2024-09-09 22:13:19 +0530

    #update

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,6 @@
 export const hostname="https://xlo-main-server.vercel.app";
 // export const hostname="http://localhost:8000";
+
+
+// ignore this
 export const clientHostname="https://xlo-main.vercel.app";
